add661a6eaa959b7a77178c3fdfcd13b94558c3b
NagaseKouichi <53267025+NagaseKouichi@users.noreply.github.com>
2020-12-03 11:11:24 +0800

    去掉关机源

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,8 +13,5 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 增加关机功能
-sed -i '$a src-git poweroff https://github.com/esirplayground/luci-app-poweroff.git' feeds.conf.default
-
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
