3a802c6690366c26a1541a7cc5a9a82390d0d07a
NagaseKouichi <53267025+NagaseKouichi@users.noreply.github.com>
2020-12-15 15:03:00 +0800

    增加Smartdns

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,6 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-ap
 
 # 增加关机功能
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
+
+# 增加Smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
