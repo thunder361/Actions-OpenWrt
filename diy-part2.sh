8b73237ed37d063ccee35c74209d42a5da3b2422
NagaseKouichi <53267025+NagaseKouichi@users.noreply.github.com>
2022-02-24 04:23:34 +0800

    删除passwall

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,21 +26,21 @@ git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/l
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
 # 获取luci-app-passwall以及缺失的依赖
-pushd package/lean
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng
-popd
+# pushd package/lean
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook
+# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng
+# popd
 
 # 增加chinadns
 #git clone https://github.com/aa65535/openwrt-chinadns.git package/chinadns
 #git clone https://github.com/aa65535/openwrt-dist-luci.git package/openwrt-dist-luci
 
 # 增加chinadns-ng
-#git clone https://github.com/NagaseKouichi/openwrt-chinadns-ng.git package/chinadns-ng
+git clone https://github.com/NagaseKouichi/openwrt-chinadns-ng.git package/chinadns-ng
 git clone -b luci https://github.com/NagaseKouichi/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
 
 # 增加dns-forwarder
